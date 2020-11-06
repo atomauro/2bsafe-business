ea7967428b917a43a4e1a6a2dd07806da80c56c4
atomauro <mauro.henaog@gmail.com>
2020-11-05 19:31:55 -0500

    change api direction

--- src/api/api.ts
+++ src/api/api.ts
@@ -17,8 +17,8 @@ const TOKEN_SMARTFIT = '875e7df451fd652e94ce6520add30404';
 //   }
 // };
 const SMARTFIT_BASE_URL = 'https://app.smartfit.com.br/api/public/v1';
-const API_2BSAFE_BASE_URL = 'https://api2bsafe.herokuapp.com';
-// const API_2BSAFE_BASE_URL = 'https://api.smartfitreserva.com';
+// const API_2BSAFE_BASE_URL = 'https://api2bsafe.herokuapp.com';
+const API_2BSAFE_BASE_URL = 'https://api.smartfitreserva.com';
 const ERRORS: any[] = [];
 
 async function callApi(url: string, options: any = {}) {
